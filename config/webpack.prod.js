2b4a82f3ccfc022b182552cbdc3e5664f0e32088
Margo <m.goncharova@group-ib.com>
2023-12-01 04:01:02 +0700

    fix(config): publicPath has been changed

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common(), {
     devtool: false,
     output: {
         path: paths.build,
-        publicPath: paths.build,
+        publicPath: '/',
         filename: '[name].bundle.js'
     },
     plugins: [
